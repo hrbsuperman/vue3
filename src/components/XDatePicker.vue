0cb63cd20f23853b0ee2a8339bb9c03ee90af377
lzy <hrbsuperman@gmail.com>
2022-10-13 11:31:18 +0800

    date-picker

--- src/components/XDatePicker.vue
+++ src/components/XDatePicker.vue
@@ -28,8 +28,9 @@
           </tr>
           </thead>
           <tbody>
-          <tr v-for="w in days">
-            <td v-for="d in w" @click="XDatePickerDay_Click(d)">
+          <tr v-for="(w,wi) in days">
+            <td v-for="d in w" @click="XDatePickerDay_Click(d,wi)"
+                :class="{ OtherMonth: (wi===0 && d > 7)||(wi >3 && d<7 )}">
               <span>{{ d }}</span>
             </td>
           </tr>
@@ -75,15 +76,14 @@ function XDateDialog_Click() {
   activeControl = true;
 }
 
-//事件冒泡，最后冒到 document
+// 事件冒泡，最后冒到 document
 // dialog区域click给activeControl true
 // 冒到 document  activeControl?不缩:缩
 function DocumentOnceClick() {
-  console.log('DocumentOnceClick')
   if (!activeControl) {
+    //先maxHeight > 0 opacity > 0 动画 再延时隐藏，
     window.setTimeout(() => {
       active.value = false;
-
       document.removeEventListener('click', DocumentOnceClick)
     }, 10)
   }
@@ -92,27 +92,48 @@ function DocumentOnceClick() {
 
 
 function Init() {
-
   let first = new Date(XDPYear.value, XDPMonth.value, 1)
   let last = new Date(XDPYear.value, XDPMonth.value + 1, 0);
 
+
   days.length = 0;
-  let monthDays = last.getDate();
+  let monthDays = last.getDate();//当月天数
   let weekIndex = 0;//当前日期的周索引
   let dayIndex = first.getUTCDay();//日期在本周的索引
   let day = 1;//日期
 
-  days.push(['', '', '', '', '', '', '']);
+  days.push([null, null, null, null, null, null, null]);
   while (day <= monthDays) {
     days[weekIndex][dayIndex] = day;
     day += 1;
     dayIndex += 1;
     if (dayIndex % 7 == 0) {
-      days.push(['', '', '', '', '', '', '']);
+      days.push([null, null, null, null, null, null, null]);
       dayIndex = 0;
       weekIndex += 1;
     }
   }
+  //补全第一周上个月最后几天
+  if (days[0][0] == null) {
+    let preLastDay = (new Date(XDPYear.value, XDPMonth.value, 0)).getDate();
+    let preCalc = 0;
+    for (let i = 5; i >= 0; i--) {
+      if (days[0][i] == null) {
+        days[0][i] = preLastDay - preCalc;
+        preCalc += 1;
+      }
+    }
+  }
+  //补全最后一周下月前几天
+  let lastWeek = days[days.length - 1];
+  let x = 1;
+  for (let i = 1; i < 7; i++) {
+    if (lastWeek[i] == null) {
+      lastWeek[i] = x;
+      x += 1;
+    }
+  }
+
 }
 
 //FOCUS
@@ -143,12 +164,18 @@ function Month_Change(i: number) {
 }
 
 //选定日期
-function XDatePickerDay_Click(day: number) {
+function XDatePickerDay_Click(day: number,widx:number) {
   XDPDay.value = day;
-  emits('update:modelValue', `${XDPYear.value}/${XDPMonth.value+1}/${day}`)
+  let month = XDPMonth.value + ((widx === 0 && day > 7) ? -1 : (widx > 3 && day <= 7 ? 1 : 0));
+  let year = XDPYear.value;
+  if(month<0){
+    month = 11;
+    year -=1;
+  }
+  if(month>11){
+    month = 0;
+    year +=1;
+  }
+  emits('update:modelValue', `${year}/${month + 1}/${day}`)
 }
 </script>
-
-<style scoped lang="less">
-
-</style>
