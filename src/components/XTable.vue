13349c608f58a28fd766dca1c2e355a317acfd13
lzy <hrbsuperman@gmail.com>
2022-10-26 16:50:20 +0800

    column fixed

--- src/components/XTable.vue
+++ src/components/XTable.vue
@@ -1,17 +1,23 @@
 <template>
   <div class="x-table-box" ref="box">
-    <div class="x-table-wrapper" :style="`width:${boxWidth}px;`">
+    <div class="x-table-wrapper" :style="`width:${boxWidth}px;`"
+         :class="{scrollLeft:scrollLeft,scrollRight:!scrollRight}">
       <div class="x-table-header" ref="header"
            :style="{overflowY: ((body?.scrollHeight||0)>(body?.clientHeight||0))?'scroll':'hidden'}">
         <!--同步overflow-y 状态-->
         <table class="x-table" :style="`width:${colFullWidth}px`">
           <colgroup>
-            <col v-for="c in columnsOption" :style="`width:${ c || colAvgWidth}px`"/>
-            <col style="width: 12px"/>
+            <col v-for="c in columnsOption" :style="`width:${ c || colAvgWidth}px`" />
+            <col style="width: 12px" />
           </colgroup>
           <thead>
           <tr>
-            <th v-for="(c,i) in columns" :class="{sticky:c.fixed}" :style="c.fixed">{{ c.label }}</th>
+            <th v-for="(c,i) in columns" :class="{
+              sticky:c.fixed,
+              fixedLeftLast:columnsFixed[i]?.fixedLeftLast,
+              fixedRightFirst:columnsFixed[i]?.fixedRightFirst
+               }" :style="c.fixed">{{ c.label }}
+            </th>
           </tr>
           </thead>
         </table>
@@ -19,11 +25,15 @@
       <div class="x-table-body" ref="body" @scroll="body_Scroll($event)">
         <table class="x-table" :style="`width:${colFullWidth}px`">
           <colgroup>
-            <col v-for="c in columnsOption" :style="`width:${ c || colAvgWidth}px`"/>
+            <col v-for="c in columnsOption" :style="`width:${ c || colAvgWidth}px`" />
           </colgroup>
           <tbody>
           <tr v-for="row in data">
-            <td v-for="(c,i) in columns" :class="{sticky:c.fixed}" :style="c.fixed">
+            <td v-for="(c,i) in columns" :class="{
+              sticky:c.fixed,
+              fixedLeftLast:columnsFixed[i]?.fixedLeftLast,
+              fixedRightFirst:columnsFixed[i]?.fixedRightFirst}"
+                :style="c.fixed">
               {{ row[c.bind] }}
             </td>
           </tr>
@@ -35,7 +45,7 @@
         <span style="margin-left: 2em">x-table.body.scrollHeight: {{ body?.scrollHeight }} </span>
         <span style="margin-left: 2em">x-table.body.clientHeight: {{ body?.clientHeight }} </span>
         <span style="margin-left: 2em">
-        {{ ((body?.scrollHeight || 0) > (body?.clientHeight || 0)) ? 'scroll' : 'hidden' }}
+        {{ ((body?.scrollHeight || 0) > (body?.clientHeight || 0)) ? "scroll" : "hidden" }}
         </span>
       </div>
     </div>
@@ -43,31 +53,37 @@
 </template>
 
 <script lang="ts" setup>
-import {ref, onMounted} from "vue";
-import type {XTableColumn} from "@/entity/component/XSupport";
+import { ref, onMounted, onBeforeMount } from "vue";
+import type { XTableColumn } from "@/entity/component/XSupport";
 
 /** 组件属性 **/
 const props = defineProps({
-  columns: {type: Array<XTableColumn>},
-  data: {type: Array<any>},
-  fixedLeftCount: {type: Number, default: 0},//左侧固定列数，固定列需要设置width
-  fixedRightCount: {type: Number, default: 0},//右侧固定列数，固定列需要设置width
-  rowNumber: {type: Boolean, default: false},
-})
+  columns: { type: Array<XTableColumn> },
+  data: { type: Array<any> },
+  fixedLeftCount: { type: Number, default: 0 },//左侧固定列数，固定列需要设置width
+  fixedRightCount: { type: Number, default: 0 },//右侧固定列数，固定列需要设置width
+  rowNumber: { type: Boolean, default: false }
+});
 /** Ref Element **/
 let box = ref<any | null>(null);
 let header = ref<any | null>(null);
 let body = ref<any | null>(null);
 /** const **/
 const columnsOption = ref<Array<number>>([]);//所有列计算后宽度，未设置宽度为0
+const columnsFixed = ref<Array<any>>([]);
 const colAvgWidth = ref<number>(0);//未设置宽度列计算平均宽度， 0||avg
 const boxWidth = ref<number>(0);//容器宽度
 const colFullWidth = ref<number>(0);//设置宽度+avg*avgCount
+/* scroll status */
+const scrollLeft = ref<boolean>(false);
+const scrollRight = ref<boolean>(false);
 
+onBeforeMount(() => {
+
+});
 onMounted(() => {
   //容器宽度
   boxWidth.value = box.value ? box.value.offsetWidth : 0;
-
   let tableColSetWidth: number = 0;//option>columns 设置宽度列合计
   let tableAvgColCount: number = 0;//未设置宽度的列平均宽度
 
@@ -85,15 +101,20 @@ onMounted(() => {
 
         let x = (props.columns || []).length - i;
         if (x <= props.fixedRightCount) {
-          c.fixed = `right:${(x == 1 ? 0 : width_Calc(props.columns.slice(i + 1)))}px`;
+          c.fixed = `right:${(x == 1 ? 0 : width_Calc((props.columns || []).slice(i + 1)))}px`;
         }
       }
     }
     columnsOption.value.push(colSetWidth);//列宽度
+    columnsFixed.value.push({
+      fixedLeftLast: props.fixedLeftCount && i === props.fixedLeftCount - 1,
+      fixedRightFirst: props.fixedRightCount && i === (props.columns || []).length - props.fixedRightCount
+    });
+
     tableColSetWidth += colSetWidth;//合计设置宽度
     tableAvgColCount += colSetWidth ? 0 : 1;//未设置宽度 Count
 
-  })
+  });
 
   //未设置宽度的列计算平均宽度  (容器宽度 - 已设置宽度) / 未设置列数
   colAvgWidth.value = boxWidth.value > tableColSetWidth ? (boxWidth.value - tableColSetWidth) / tableAvgColCount : 0;
@@ -115,13 +136,13 @@ function width_Calc(columns: any) {
       if (typeof (columns.width) === "number") {
         width = columns.width;
       } else {
-        let unit = columns.width.toString().replace(/[0-9]+/g, '');
+        let unit = columns.width.toString().replace(/[0-9]+/g, "");
         switch (unit) {
-          case 'px':
-            width = parseInt(columns.width.replace(/px/g, ''));
+          case "px":
+            width = parseInt(columns.width.replace(/px/g, ""));
             break;
-          case '%':
-            width = boxWidth.value * (parseInt(columns.width.replace(/%/g, '')) / 100)
+          case "%":
+            width = boxWidth.value * (parseInt(columns.width.replace(/%/g, "")) / 100);
             break;
         }
       }
@@ -133,6 +154,9 @@ function width_Calc(columns: any) {
 //横向滚动同步给 t-table-header
 function body_Scroll(e: any) {
   header.value.scrollLeft = body.value.scrollLeft;
+  scrollLeft.value = header.value.scrollLeft > 0;
+  console.log(body.value.scrollWidth - body.value.clientWidth - body.value.scrollLeft);
+  scrollRight.value = body.value.scrollWidth - body.value.clientWidth - body.value.scrollLeft < 10;
 }
 </script>
 
@@ -191,18 +215,40 @@ function body_Scroll(e: any) {
         background-image: linear-gradient(#e8eaec, #e8eaec), linear-gradient(#e8eaec, #e8eaec);
         background-repeat: no-repeat;
         background-size: 1px 100%, 100% 1px;
-        background-position: 100% 0, 100% 100%;
+        //background-position: 100% 0, 100% 100%;
         z-index: 10;
+
       }
 
       td {
         background-color: #fff;
       }
 
-      .fixedLeftLast {
-        border-right: 1px solid #000;
-      }
+
     }
+  }
+
+  .fixedLeftLast, .fixedRightFirst {
+    &::after {
+      transition: box-shadow var(--animationSpeed);
+      position: absolute;
+      content: ' ';
+      top: 0;
+      width: 10px;
+      height: 100%;
+    }
+  }
+  .fixedLeftLast::after {
+    right: -10px;
+  }
+  .fixedRightFirst::after {
+    left: -10px;
+  }
+  .scrollLeft .fixedLeftLast::after {
+    box-shadow: inset 10px 0 8px -8px #00000026;
+  }
+  .scrollRight .fixedRightFirst::after {
+    box-shadow: inset -10px 0 8px -8px #00000026;
   }
 }
 </style>
