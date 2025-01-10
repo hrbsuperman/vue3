46a948e564aab02f776a3ab3540d553c5cbaa4ae
lzy <hrbsuperman@gmail.com>
2025-01-10 23:05:11 +0800

    init

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,28 @@
-import {defineAsyncComponent} from 'vue'
+import {markRaw,defineAsyncComponent} from 'vue'
+import Home from '../views/system/home.vue'
 
 
 const routers: any = {
+    "home": Home,                                                                                      /*home*/
+    "customer": defineAsyncComponent(() => import('@/views/system/login.vue')),                 /**/
+    "Test":defineAsyncComponent(() => import('@/views/example/Table.vue')),                     /**/
 
+
+    //Example
+    "Form":defineAsyncComponent(() => import('@/views/example/Form.vue')),                      /*表单*/
+    "Table":defineAsyncComponent(() => import('@/views/example/Table.vue')),                    /*Table*/
+    "FlexGrid":defineAsyncComponent(() => import('@/views/example/FlexGrid.vue')),              /*FlexGrid*/
+    "StyleComponent":defineAsyncComponent(() => import('@/views/example/StyleComponent.vue')),  /*样式*/
+    "FontIcon":defineAsyncComponent(() => import('@/views/example/FontIcon.vue')),              /*字体图标*/
+    "Dialog":defineAsyncComponent(() => import('@/views/example/Dialog.vue')),                  /*模态*/
+    "403":defineAsyncComponent(() => import('@/views/example/exception/403.vue')),              /*403*/
+    "404":defineAsyncComponent(() => import('@/views/example/exception/404.vue')),              /*404*/
+    "500":defineAsyncComponent(() => import('@/views/example/exception/500.vue')),              /*500*/
+
+
+    "Loading":defineAsyncComponent(() => import('@/views/example/Loading.vue')),                /*Loading*/
+
+    "a8":defineAsyncComponent(() => import('@/views/demo/a8.vue'))
 };
 
 export default routers
---
+++ src/views/system/home.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="home">
+    <div class="head">
+      <div class="photo">
+        <img alt="头像" src="https://q1.qlogo.cn/g?b=qq&nk=190848757&s=640" style="width: 75px;border-radius: 100%"/>
+      </div>
+      <div class="textMessage">
+        <p style="font-size: 1.2em;color:var(--gray-9);">早安, Admin, 开始您一天的工作吧！</p>
+        <p style="color:var(--gray-8)">今日晴，20℃ - 32℃！</p>
+      </div>
+      <div class="work" :style="{width:works.length* 80+'px'}">
+        <div v-for="w in works">
+          <div class="desc">{{ w.desc }}</div>
+          <div class="number">{{ w.num }}</div>
+        </div>
+
+      </div>
+    </div>
+    <div class="content">
+        <div style="height: 200px;background-color: #fff;">
+
+        </div>
+    </div>
+
+  </div>
+</template>
+
+<script setup lang="ts">
+import {reactive} from "vue";
+
+const works: Array<any> = reactive([{desc: '待办', num: '2/10'}, {desc: '项目', num: '3'}]);
+</script>
+
+<style scoped lang="less">
+.home {
+  .head {
+    background-color: #fff;
+    padding: 1.5em;
+    display: flex;
+    flex-shrink: 0;
+
+    .textMessage {
+      width: 100%;
+      padding: 0.7em 0 0 2em;
+    }
+
+    .work {
+      flex-shrink: 0;
+      display: flex;
+      justify-content: space-between;
+      text-align: right;
+      margin-top: 10px;
+      padding-right: 20px;
+
+      .desc {
+        color: var(--gray-6)
+      }
+
+      .number {
+        font-size: 20px;
+      }
+    }
+  }
+  .content{
+    padding:1.2em;
+  }
+
+}
+</style>
